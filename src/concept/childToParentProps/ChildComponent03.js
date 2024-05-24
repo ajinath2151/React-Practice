86d435ea753f63892221d5847b1bf7e534ad3d75
Ajinath <ajinath2151@gmail.com>
2024-05-24 17:04:48 +0530

    using button child to parent props sending

--- src/concept/childToParentProps/ChildComponent03.js
+++ src/concept/childToParentProps/ChildComponent03.js
@@ -4,16 +4,22 @@ const ChildComponent03 = (props) => {
 
     const [fname, setFname] = useState('');
 
-    const handleSubmit = (e) => { 
+  //   const handleSubmit = (e) => { 
+  //   e.preventDefault();
+  //   props.getData(fname);
+  //  }
+    const handleClick = (e) => { 
     e.preventDefault();
     props.getData(fname);
    }
   return (
     <div>
-      <form onSubmit={handleSubmit} action="">
+      <form action="">
+      {/* <form onSubmit={handleSubmit} action=""> */}
         <h1>
         <input type="text" name="" value={fname} id="" onChange={(e)=>{setFname(e.target.value)}} />
-        <button>Submit</button>
+        {/* <button>Submit</button> */}
+        <button onClick={handleClick}>Submit</button>
         </h1>
             
       </form>
