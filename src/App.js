3c227a38e7418b33f45cfbdefacdfd63276ceeaa
Ajinath <ajinath2151@gmail.com>
2024-05-25 22:49:45 +0530

    child to parent data passing in reactjs

--- src/App.js
+++ src/App.js
@@ -134,6 +134,7 @@ import ValidationWithNpm from "./Components/ValidationWithNpm";
 import ParentProps1 from "./concept/parentToChildProps/ParentProps1";
 import ParentComponents03 from "./concept/childToParentProps/ParentComponents03";
 import ParentComponents04 from "./concept/childToParentProps/ParentComponents04";
+import ParentComponents05 from "./concept/childToParentProps/ParentComponents05";
 
 
 
@@ -288,7 +289,8 @@ function App() {
         {/* <ValidationWithNpm /> */}
         {/* <ParentProps1 /> */}
         {/* <ParentComponents03 /> */}
-        <ParentComponents04 />
+        {/* <ParentComponents04 /> */}
+        <ParentComponents05 />
 
     </>
   );
